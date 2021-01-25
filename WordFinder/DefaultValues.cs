2e21d2586a2b52a4d0f599e5a251b4956879d8cb
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-01-25 13:07:27 +0100

    Implemented Loading of File for words

---
+++ WordFinder/DefaultValues.cs
@@ -0,0 +1,17 @@
+namespace WordFinder
+{
+	public class DefaultValues
+	{
+		// German Version
+		public static readonly string[] WordFinderWords =
+		{
+			"angst", "atmen", "beten", "bombe", "danke",
+			"draht", "druck", "drück", "farbe", "fehlt",
+			"ferse", "kabel", "knall", "knapp", "knopf",
+			"leere", "legal", "lehre", "mathe", "matte",
+			"panik", "pieps", "rauch", "ruhig", "saite",
+			"sehne", "seite", "sende", "strom", "super",
+			"timer", "übrig", "verse", "warte", "zange",
+		};
+	}
+}
